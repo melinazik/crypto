962e6ab138435d5352b7ac69fb9a049812c941d1
melinaz@live.com <melinaz@live.com>
2021-03-06 22:17:44 +0200

    RC4 algorithm fix Encryption OK Decryption OK

--- ex1.py
+++ ex1.py
@@ -3,13 +3,14 @@ import numpy as np
 # key scheduling algorithm (KSA)
 def KSA(key):
     keyLen = len(key)
+    key = [ord(x) for x in key]
     
-    S = list(range(256))
+    S = [x for x in range(256)]
 
     j = 0
 
     for i in range(256):
-        j = (j + S[i] + key[i % keyLen]) % 256
+        j = (j + S[i] + int(key[i % keyLen])) % 256
 
         # swap values
         S[i], S[j] = S[j], S[i]
@@ -18,70 +19,88 @@ def KSA(key):
     return S
 
 # Pseudo-random generation algorithm (PRGA)
-
 # S : permuation array
-# n : length of plain text
-def PRGA(S, n):
+
+#prga for encryption
+def PRGAEncrypt(S, plainText):
     i = 0
     j = 0
 
     # key array 
-    key = []
+    cipherText = []
 
-    while n > 0:
-        n = n - 1
+    for c in plainText:
         i = (i + 1) % 256  
         j = (j + S[i]) % 256
         
         S[i], S[j] = S[j], S[i]
 
-        K = S[(S[i] + S[j]) % 256]
-        key.append(K)
+        hexed = ("%02X" % ord(chr(S[(S[i] + S[j]) % 256] ^ ord(c))))
+
+        cipherText.append(hexed)
+
+    return cipherText
+
+#prga for encryption
+def PRGADecrypt(S, cipherText):
+    i = 0
+    j = 0
+
+    # key array 
+    plainText = ''
+
+    for c in cipherText:
+        i = (i + 1) % 256  
+        j = (j + S[i]) % 256
         
+        S[i], S[j] = S[j], S[i]
+        
+        char = chr(int(c,16) ^ S[(S[i] + S[j]) % 256])
 
-    return key
+        plainText += char
 
-def getKeyStream(key, n):
-    S = KSA(key)
-    return PRGA(S, n)
+    return plainText
+
+
+#     return cipher
+def listToString(list):  
     
+    # initialize an empty string 
+    str = ""  
+    
+    # traverse in the string   
+    for element in list:  
+        str += element 
+    
+    # return string   
+    return str
+
+
 def encrypt(key, plainText):
-    key = [ord(c) for c in key]
-
-    keyStream = np.array(getKeyStream(key, len(plainText)))
-    plainText = np.array([ord(i) for i in plainText])
-    cipher = keyStream ^ plainText
-    return cipher.astype(np.uint8).data.hex()
-
-    # result = []
-    # i = 0
-    # # xor and hex values
-    # for c in plainText:
-    #     value = ("%02X" % (ord(c) ^ keyStream[i]))  # XOR and taking hex
-    #     result.append(value.upper())
-    #     i = i + 1
+    S = KSA(key)
+    cipherText = PRGAEncrypt(S, plainText)
 
 
-    # return ''.join(result)
+    return listToString(cipherText)
 
 def decrypt(key, cipherText):
-    # convert ciphertext from hex to plain ASCII
+    S = KSA(key)
+    cipherList=cipherText.split(' ')
     
+    plainText = PRGADecrypt(S, cipherList)
 
-    result = encrypt(key, cipherText)
-
-    return result
-
+    return plainText
 
 key = 'HOUSE'
 plainText = 'MISTAKES ARE AS SERIOUS AS THE RESULTS THEY CAUSE'
+print('PLAIN TEXT:' , plainText)
+print('KEY:' , key)
 
 encrypted = encrypt(key, plainText)
+print('\nENCRYPTED:', encrypted)
 
-print('plaintext: ' + plainText)
 
-print('ciphertext: ' + encrypted.upper())
-
-cipherText = '15dda667fe6add1a49167a12a40cf54b42d4cd7014e2ef5c0a87a3010b47eb7919777ddb578614557294aeafd9621c8b19'.upper()
+# cipherText = '15DDA667FE6ADD1A49167A12A40CF54B42D4CD7014E2EF5C0A87A3010B47EB7919777DDB578614557294AEAFD9621C8B19'
+cipherText = " ".join(encrypted[i:i+2] for i in range(0, len(encrypted), 2))
 decrypted = decrypt(key, cipherText)
-print ('\ndecrypted:', decrypted.upper())
+print('\nDECREPTED:' , decrypted)
