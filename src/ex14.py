12858ca6a5ce81d592ee17c14a8ad5bf1e023a09
melinaz@live.com <melinaz@live.com>
2021-04-05 19:50:39 +0300

    Tonelli Shanks - initialization

---
+++ src/ex14.py
@@ -0,0 +1,9 @@
+''' 
+    Tonelli - shanks Algorithm
+    (Modular Arithmetic)
+    Exercise 14
+    
+    Melina Zikou (2021)
+'''
+# x ^ 2 = a mod p
+# x ^ 2 = 17592194433025 mod 309485009821345068724781063
