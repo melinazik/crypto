e6099fdf3a41c57ac1b59cad0bda6ac8a98ca93d
melinaz@live.com <melinaz@live.com>
2021-06-10 00:10:12 +0300

    First attempt on probability problem

--- src/ex36.py
+++ src/ex36.py
@@ -5,4 +5,37 @@
     
     Melina Zikou (2021)
 '''
+import math
+import random
 
+# find all divisors of integer number
+def divisors(n):
+    i = 1
+    factors = []
+    while i <= n :
+        if (n % i==0) :
+            factors.append(i)
+        i = i + 1
+
+    return factors
+
+def probability(L, n):
+
+    for i in L:
+        if (len(bin(i)) - 2 >= math.floor(n/2) and (len(bin(i)) - 2 <= math.floor(n/2) + 1 or len(bin(i)) - 2 <= math.floor(n/2) - 1)):
+            return 1
+
+        return -1
+
+count = 0
+K = 1000
+for i in range(0,K):
+    n = 10
+    m = random.getrandbits(n)
+    L = divisors(m)
+
+    if(probability(L, n) == 1):
+        count = count + 1
+
+
+print(count/ K)
